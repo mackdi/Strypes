3a6f035a7fb3fcba7fdb79a6c558b45d65913d03
dinko <dinkokovachev@gmail.com>
2022-08-06 12:52:46 +0300

    version 1

---
+++ Homework/Week2/Problem_1/Problem_1a/bmi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    float mass, height, bmi;
+
+    printf("Please enter your mass: ");
+    scanf("%f", &mass);
+    printf("Please enter your height: ");
+    scanf("%f", &height);
+
+    bmi = mass / pow(height, 2);
+
+    printf("Your BMI is: %f\n", bmi);
+
+    return 0;
+}

dbcda8924ab7534cb410bf835eac47f7bb79887b
dinko <dinkokovachev@gmail.com>
2022-08-07 15:42:44 +0300

    fix tripple XOR

--- Homework/Week2/Problem_4/xorTreeNumbers.c
+++ Homework/Week2/Problem_4/xorTreeNumbers.c
@@ -4,9 +4,8 @@ int main(){
     int num1, num2, num3, result;
 
     scanf("%d %d %d", &num1, &num2, &num3);
-    //result = num1 ^ num2 ^ num3; // not working when 3 of 1s
-    result = (num1?1:0)+(num2?1:0)+(num3?1:0) == 1; // when odd numbers of 1s
-
+    result = num1 ^ num2 ^ num3; //  when odd times of true
+    //result = (num1?1:0)+(num2?1:0)+(num3?1:0) == 1; // when only one true
     printf("%d\n", result);
     return 0;
 

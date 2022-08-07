9079f225e33712a31ba8e42091f03b343c264833
dinko <dinkokovachev@gmail.com>
2022-08-07 12:26:10 +0300

    problem 2 a-ok, b-problem in formula

---
+++ Homework/Week2/Problem_2/Problem_2b/waterCylinderHorizontal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+#define _USE_MATH_DEFINES
+
+int main(){
+    float R, L, H, V, A;
+    printf("Pleas enter radius: ");
+    scanf("%f", &R);
+    printf("Pleas enter water level: ");
+    scanf("%f", &H);
+    printf("Please enter water height: ");
+    scanf("%f", &L);
+    
+    //A = ((R*R)*acos((R - H)/R)-(R - H) * sqrt((2*R)* H)-(H*H));
+    A = ((R * R) * acos((R-H)/R) - (R-H) * sqrt((2*R)*H - (H*H)));
+    V = A * L;
+    printf("Volume of the water is: %f m3\n", V);
+
+return 0;
+}

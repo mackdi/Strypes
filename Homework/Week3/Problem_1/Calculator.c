e147090f36426c712cfdf717b1218ec17b154e4d
dinko <dinkokovachev@gmail.com>
2022-08-11 21:05:38 +0300

    Problem_1

---
+++ Homework/Week3/Problem_1/Calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    float num1, num2;
+    char oper;
+    //TODO EOF
+    while (EOF)
+    {
+    scanf("%f %c %f", &num1,&oper,&num2);
+    // ca be done with switch
+    if(oper == '+') printf("%.2f\n", num1 + num2);
+    if(oper == '-') printf("%.2f\n", num1 - num2);
+    if(oper == 'x') printf("%.2f\n", num1 * num2);
+    if(oper == '/') printf("%.2f\n", num1 / num2);
+    }
+    return 0;
+}

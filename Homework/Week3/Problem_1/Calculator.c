1fec636a03cc4ac36e5a61bd7f45062f41389dc1
dinko <dinkokovachev@gmail.com>
2022-08-14 14:03:02 +0300

    ok

--- Homework/Week3/Problem_1/Calculator.c
+++ Homework/Week3/Problem_1/Calculator.c
@@ -6,9 +6,10 @@ int main(){
     //TODO wrong input check
     while (scanf("%f %c %f", &num1,&oper,&num2)!=EOF)
     {
-    // scanf("%f %c %f", &num1,&oper,&num2);
-    // ca be done with switch
+
+    
     //typeof(num1);
+    // ca be done with switch
     if(oper == '+') printf("%.2f\n", num1 + num2);
     if(oper == '-') printf("%.2f\n", num1 - num2);
     if(oper == 'x') printf("%.2f\n", num1 * num2);

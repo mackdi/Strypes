c5dfe4cefaf7d4d96c2c2b00556b84f8756a8bbf
dinko <dinkokovachev@gmail.com>
2022-08-14 12:25:44 +0300

    scanf in while

--- Homework/Week3/Problem_1/Calculator.c
+++ Homework/Week3/Problem_1/Calculator.c
@@ -4,9 +4,9 @@ int main(){
     float num1, num2;
     char oper;
     //TODO EOF
-    while (EOF)
+    while (scanf("%f %c %f", &num1,&oper,&num2)!=EOF)
     {
-    scanf("%f %c %f", &num1,&oper,&num2);
+    // scanf("%f %c %f", &num1,&oper,&num2);
     // ca be done with switch
     if(oper == '+') printf("%.2f\n", num1 + num2);
     if(oper == '-') printf("%.2f\n", num1 - num2);

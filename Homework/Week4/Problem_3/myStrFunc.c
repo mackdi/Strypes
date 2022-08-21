84b30cb183153eca99acea5382267f0528f18a3d
dinko <dinkokovachev@gmail.com>
2022-08-21 21:59:18 +0300

    myStren

--- Homework/Week4/Problem_3/myStrFunc.c
+++ Homework/Week4/Problem_3/myStrFunc.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+
+size_t myStrlen(char * string);
+
 
 int main(){
 
+    char a[500]="2sfsdfwertwerwtegdfgdfgdfgdfterert";
+    printf("%ld\n", myStrlen(a));
     return 0;
-    
+}
+
+size_t myStrlen(char * string){
+
+    int sum = 0;
+
+    // With for loop
+    // int sum = 0;
+    // for (size_t i = 0; string[i] != '\0'; i++)
+    // {
+    //     sum++;
+    // }
+
+    int i = 0;
+    while (string[i] != '\0') /* '\0' - means the end of the string */
+    {
+        sum++;
+        i++;
+    }
+    return sum;
 }

0a737b503ffd0481799d636195b138555722de67
dinko <dinkokovachev@gmail.com>
2022-09-09 17:45:23 +0300

    ok

--- Homework/Week6,7/Problem_7/search.c
+++ Homework/Week6,7/Problem_7/search.c
@@ -1,36 +1,48 @@
 #include <stdio.h>
 
-unsigned int BinSearch(unsigned int* pArray, unsigned int uArraySize, unsigned int uValue);
-void Bubble(int* pArray, unsigned int uLen);
-void swap(int* a, int* b);
+unsigned int BinSearch(unsigned int *pArray, unsigned int uArraySize, unsigned int uValue);
+void Bubble(int *pArray, unsigned int uLen);
+void swap(int *a, int *b);
 
 int main()
 {
-    unsigned int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-    printf("Index is %d\n", BinSearch(arr, 10, 5));
-	return 0;
+    int n, m;
+    printf("Please enter the size of the array: ");
+    scanf("%d", &n);
+    unsigned int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Please enter value for position %d: ", i);
+        scanf("%d", &arr[i]);
+    }
+    printf("Please enter searched value: ");
+    scanf("%d", &m);
+    printf("Index is %d\n", BinSearch(arr, n, m));
+    return 0;
 }
 
-unsigned int BinSearch(unsigned int* pArray, unsigned int uArraySize, unsigned int uValue)
+unsigned int BinSearch(unsigned int *pArray, unsigned int uArraySize, unsigned int uValue)
 {
     unsigned int max = uArraySize;
     unsigned int min = 0;
     unsigned int point = max / 2;
-    unsigned int ret=0;
+    unsigned int ret = 0;
     Bubble(pArray, uArraySize);
     while (pArray[point] != uValue)
     {
         if (pArray[point] > uValue)
         {
             max = point;
-            point = (min+max)/2;
-            if (pArray[point] == uValue) ret = point;
+            point = (min + max) / 2;
+            if (pArray[point] == uValue)
+                ret = point;
         }
         else if (pArray[point] < uValue)
         {
             min = point;
-            point = (min + (max / 2))-1;
-            if (pArray[point] == uValue) ret = point;
+            point = (min + (max / 2)) - 1;
+            if (pArray[point] == uValue)
+                ret = point;
         }
         else
         {
@@ -40,7 +52,7 @@ unsigned int BinSearch(unsigned int* pArray, unsigned int uArraySize, unsigned i
     }
     return ret;
 }
-void Bubble(int* pArray, unsigned int uLen)
+void Bubble(int *pArray, unsigned int uLen)
 {
     if (uLen > 1)
     {
@@ -55,9 +67,8 @@ void Bubble(int* pArray, unsigned int uLen)
             }
         }
     }
-
 }
-void swap(int* a, int* b)
+void swap(int *a, int *b)
 {
     int temp = *a;
     *a = *b;
